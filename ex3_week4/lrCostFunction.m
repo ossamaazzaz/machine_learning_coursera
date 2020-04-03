8af74c4e64a7e2bfd4dabc59c4828ad37999a97d
ossamaazzaz <o.azzazrahmani@esi-sba.dz>
2020-04-03 01:56:30 +0100

    ex3_week4PA - Regularized Logistic Regression

--- ex3_week4/lrCostFunction.m
+++ ex3_week4/lrCostFunction.m
@@ -36,13 +36,13 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+tmpTheta = theta;
+tmpTheta(1) = 0;
 
-
-
-
-
-
-
+J = (-1 / m) * sum(y.*log(sigmoid(X * theta)) + (1 - y).*log(1 - sigmoid(X * theta))) + (lambda / (2 * m))*sum(tmpTheta.^2);
+temp = sigmoid (X * theta);
+error = temp - y;
+grad = (1 / m) * (X' * error) + (lambda/m)*tmpTheta;
 
 
 % =============================================================
